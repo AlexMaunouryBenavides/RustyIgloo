3905c950f15b5f8be7ad888b43fab27f8c61b3be
AlexMaunouryBenavides <alexandremaunoury@yahoo.fr>
2023-11-25 12:46:33 +0100

    ajout sitemap contact

--- siteMap.cjs
+++ siteMap.cjs
@@ -4,6 +4,7 @@ const hostname = 'https://www.example.com';
 const urls = [
   { url: '/', changefreq: 'daily', priority: 1 },
   { url: '/projets', changefreq: 'monthly', priority: 0.8 }
+  { url: '/contact', changefreq: 'monthly', priority: 0.8 }
   
   // Add additional pages here
 ];
